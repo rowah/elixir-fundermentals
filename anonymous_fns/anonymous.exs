5866bc4ace2b23656881cc120d2ebf28eec1d705
Rowa <jrowah@gmail.com>
2023-02-09 12:23:03 +0300

    anonymous functions

--- anonymous_fns/anonymous.exs
+++ anonymous_fns/anonymous.exs
@@ -9,6 +9,8 @@ iex(8)> list_concat = fn [a, b], [c, d] -> [a, b, c, d] end
 iex(9)> list_concat.([:a, :b], [:c, :d])
 [:a, :b, :c, :d]
 
+list_concat = fn (a, b), (c, d) -> IO.puts [a, b, c, d] end
+
 def concat_lists(list1, list2) do
   list1 ++ list2
 end
@@ -26,3 +28,20 @@ iex(12)> pair_tuple_to_list = fn {a, b} -> [a, b] end;
 #Function<44.97283095/1 in :erl_eval.expr/5>
 iex(13)> pair_tuple_to_list.({1234, 5678})
 [1234, 5678]
+
+
+
+
+iex(1)> handle_file = fn
+  ...(1)> {:ok, file} -> "Read data: #{IO.read(file, :line)}"
+  ...(1)> {_, error} -> "Error: #{:file.format_error(error)}"
+  ...(1)> end
+  #Function<44.97283095/1 in :erl_eval.expr/5>
+  iex(2)> handle_open.(File.open("elixir-fundermentals/codewars/war001.exs"))
+  ** (CompileError) iex:2: undefined function handle_open/0
+      (stdlib 3.13) lists.erl:1354: :lists.mapfoldl/3
+  iex(2)> handle_file.(File.open("elixir-fundermentals/codewars/war001.exs"))
+  "Error: no such file or directory"
+  iex(3)> handle_file.(File.open("codewars/war001.exs"))
+  "Read data: # Create a function that takes an integer as an argument and returns \"Even\" for even numbers or \"Odd\" for odd numbers.\n"
+  iex(4)>
