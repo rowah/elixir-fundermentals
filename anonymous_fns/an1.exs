72bdaaf1f498b1188c40f6da79f8eeb5fbe92232
Rowa <jrowah@gmail.com>
2023-02-12 21:06:21 +0300

    fn returning other fns

--- anonymous_fns/an1.exs
+++ anonymous_fns/an1.exs
@@ -8,3 +8,16 @@ outer_fn.().()
 other_fn = outer_fn.()
 
 other_fn.()
+
+
+greeter = fn name -> (fn -> IO.puts "Hello, #{name}" end) end
+greeter.("Dave").() #Hello, Dave
+dave_greeter = greeter.("Dave")
+dave_greeter.() #Hello, Dave
+
+
+# Write a function prefix that takes a string. It should return a new function that takes a second string. When that second function is called, it will return a string containing the first string, a space, and the second string.
+
+string_one = fn str -> (fn another_str -> IO.puts "#{str} #{another_str}" end) end
+string_two = string_one.("James")
+string_two.("Rowa") # James Rowa
