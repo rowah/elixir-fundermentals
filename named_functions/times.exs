7730a4a740ee69ca949b69f8c64eceabb51e838b
Rowa <jrowah@gmail.com>
2023-02-13 09:41:14 +0300

    module syntaxes

---
+++ named_functions/times.exs
@@ -0,0 +1,18 @@
+# defmodule Times do
+#   def triple(n) do
+#     n * 3
+#   end
+
+# end
+
+
+
+#can be written as
+defmodule Times do
+  def triple(n) do: n * 3
+
+end
+
+
+#OR
+# defmodule Times, do: (def triple(n), do: n * 3)
