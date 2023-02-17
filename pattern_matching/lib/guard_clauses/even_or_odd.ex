f3f67a041e0aba2da3f31e40f82b92df2274f8c6
Rowa <jrowah@gmail.com>
2023-02-17 12:05:39 +0300

    using guard clauses

--- pattern_matching/lib/guard_clauses/even_or_odd.ex
+++ pattern_matching/lib/guard_clauses/even_or_odd.ex
@@ -1,5 +1,9 @@
 defmodule GuardClauses.EvenOrOdd do
+  require Integer
   def odd_or_even(number) when Integer.is_even(number), do: "even"
   def odd_or_even(number) when Integer.is_odd(number), do: "odd"
 
 end
+
+IO.puts GuardClauses.EvenOrOdd.odd_or_even(11)
+IO.puts GuardClauses.EvenOrOdd.odd_or_even(12)
